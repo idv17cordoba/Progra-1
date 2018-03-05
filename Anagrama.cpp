c68f6ed432b228f7d867a0937458ab48039636df
idv17cordoba <31976387+idv17cordoba@users.noreply.github.com>
2018-03-04 22:21:03 -0600

    Ejercicio Anagrama

---
+++ Anagrama.cpp
@@ -0,0 +1,46 @@
+#include "Libreria.h"
+
+
+int main () {
+  string line;
+
+  vector<string>words;
+  vector<string>anagram;
+
+  ifstream myfile ("Anagrama.txt");
+  if (myfile.is_open())
+  {
+    while ( getline (myfile,line) )
+    {
+        words.push_back(line);
+    }
+    myfile.close();
+  }
+
+  else cout << "Unable to open file";
+
+for(int i=0;i<words.size();i++)
+  {
+    cout<<words[i]<<endl;
+    anagram.push_back(words[i]);
+    sort(anagram[i].begin(),anagram[i].end());
+
+  }
+int i=0;
+while(i<words.size()-1)
+{
+if(anagram[i] == anagram[i+1])
+{
+        cout<<words[i]<< " es anagrama de "<<words[i+1]<<endl;
+
+}
+else
+    {
+        cout<<words[i]<< " no es anagrama de "<<words[i+1]<<endl;
+    }
+    i++;
+}
+
+
+  return 0;
+}
